ee3a93a60c1a7ed3930dfb166cf6209463deb79b
Pang Khang Jer <pangkhangjer@gmail.com>
2022-03-22 20:50:53 +0000

    add leftover changes

---
+++ Assets/TestMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float walkingSpeed;
+    float x;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        //Move(x);
+    }
+
+    void Move(float horizontalInput)
+    {
+        rb.velocity = new Vector2(horizontalInput * walkingSpeed, rb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + x * walkingSpeed * Time.deltaTime);
+    }
+}
